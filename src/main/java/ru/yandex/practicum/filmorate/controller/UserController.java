b015f82370ff6bfc3c0145fa90a92519b1a560f2
bannovdaniil <baddan@mail.ru>
2022-06-18 21:25:40 +0300

    #filmorate friends likes -#3 fix: UserService

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -12,10 +12,7 @@ import ru.yandex.practicum.filmorate.service.UserService;
 import ru.yandex.practicum.filmorate.storage.UserStorage;
 
 import javax.validation.Valid;
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 @RestController
 @RequestMapping("/users")
@@ -53,55 +50,28 @@ public class UserController {
     public void addFriend(
             @PathVariable("id") Long userId,
             @PathVariable("friendId") Long friendId) throws UserGetException {
-        User user = userStorage.getUserById(userId);
-        if (userStorage.getUserById(friendId) != null) {
-            userService.addFriend(user, friendId);
-        }
+        userService.addFriend(userId, friendId);
     }
 
     @DeleteMapping("{id}/friends/{friendId}")
     public void removeFriend(
             @PathVariable("id") Long userId,
             @PathVariable("friendId") Long friendId) throws UserGetException, UserDeleteException {
-        User user = userStorage.getUserById(userId);
-        if (userStorage.getUserById(friendId) != null) {
-            if (!userService.removeFriend(user, friendId)) {
-                throw new UserDeleteException("Can't delete friend.");
-            }
-        }
+        userService.removeFriend(userId, friendId);
     }
 
     @GetMapping("{id}/friends")
     public List<User> getFriendsList(@PathVariable("id") Long userId) throws UserGetException {
-        User user = userStorage.getUserById(userId);
-        List<User> resultFriendsList = new ArrayList<>();
-        Set<Long> friendsIds = userService.getFriendList(user);
-
-        for (Long friendId : friendsIds) {
-            resultFriendsList.add(userStorage.getUserById(friendId));
-        }
-
-        return resultFriendsList;
+        return userService.getFriendList(userId);
     }
 
     @GetMapping("{id}/friends/common/{otherId}")
     public List<User> getCrossFriend(
             @PathVariable("id") Long userId,
             @PathVariable("otherId") Long otherId) throws UserGetException {
-        User user = userStorage.getUserById(userId);
-        User other = userStorage.getUserById(otherId);
-
-        Set<Long> friendsIdsUnion = new HashSet<>(userService.getFriendList(user));
-        friendsIdsUnion.retainAll(userService.getFriendList(other));
-
-        List<User> resultFriendsList = new ArrayList<>();
-
-        for (Long friendId : friendsIdsUnion) {
-            resultFriendsList.add(userStorage.getUserById(friendId));
-        }
-
-        return resultFriendsList;
+        return userService.getCrossFriendList(userId, otherId);
     }
+
 
 }
 
--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -1,21 +1,72 @@
 package ru.yandex.practicum.filmorate.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.exceptions.UserDeleteException;
+import ru.yandex.practicum.filmorate.exceptions.UserGetException;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.storage.UserStorage;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Service
 public class UserService {
-    public void addFriend(User user, long id) {
-        user.addFriend(id);
+    private final UserStorage userStorage;
+
+    @Autowired
+    public UserService(UserStorage userStorage) {
+        this.userStorage = userStorage;
     }
 
-    public boolean removeFriend(User user, long id) {
-        return user.removeFriend(id);
+    public void addFriend(Long userId, Long friendId) throws UserGetException {
+        User user = userStorage.getUserById(userId);
+        if (userStorage.getUserById(friendId) != null) {
+            user.addFriend(friendId);
+        }
     }
 
-    public Set<Long> getFriendList(User user) {
+    public void removeFriend(Long userId, Long friendId) throws UserGetException, UserDeleteException {
+        User user = userStorage.getUserById(userId);
+        if (userStorage.getUserById(friendId) != null) {
+            if (!user.removeFriend(friendId)) {
+                throw new UserDeleteException("Can't delete friend.");
+            }
+        }
+    }
+
+    private Set<Long> getFriendIdList(User user) {
         return user.getFriends();
     }
+
+    public List<User> getFriendList(Long userId) throws UserGetException {
+        User user = userStorage.getUserById(userId);
+        List<User> resultFriendsList = new ArrayList<>();
+        Set<Long> friendsIds = getFriendIdList(user);
+
+        for (Long friendId : friendsIds) {
+            resultFriendsList.add(userStorage.getUserById(friendId));
+        }
+
+        return resultFriendsList;
+    }
+
+    public List<User> getCrossFriendList(Long userId, Long otherId) throws UserGetException {
+        User user = userStorage.getUserById(userId);
+        User other = userStorage.getUserById(otherId);
+
+        Set<Long> friendsIdsUnion = new HashSet<>(getFriendIdList(user));
+        friendsIdsUnion.retainAll(getFriendIdList(other));
+
+        List<User> resultFriendsList = new ArrayList<>();
+
+        for (Long friendId : friendsIdsUnion) {
+            resultFriendsList.add(userStorage.getUserById(friendId));
+        }
+
+        return resultFriendsList;
+    }
+
 }
