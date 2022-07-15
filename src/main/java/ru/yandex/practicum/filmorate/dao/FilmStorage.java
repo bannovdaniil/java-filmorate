8e829408c5be7e57808ba0f0b495ff76e7b1bda9
bannovdaniil <baddan@mail.ru>
2022-07-15 10:51:15 +0300

    #filmorate !11 рефакторинг

--- src/main/java/ru/yandex/practicum/filmorate/dao/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/FilmStorage.java
@@ -1,4 +1,4 @@
-package ru.yandex.practicum.filmorate.storage;
+package ru.yandex.practicum.filmorate.dao;
 
 import ru.yandex.practicum.filmorate.dto.DtoFilm;
 import ru.yandex.practicum.filmorate.exceptions.*;
