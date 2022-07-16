2d7c4eb00e6a426bd47b4e9883ff5f9b918b6eea
bannovdaniil <baddan@mail.ru>
2022-07-16 19:03:58 +0300

    #filmorate !11 Genres cache

--- src/main/java/ru/yandex/practicum/filmorate/dao/impl/GenreDaoStorageImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/impl/GenreDaoStorageImpl.java
@@ -2,61 +2,50 @@ package ru.yandex.practicum.filmorate.dao.impl;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.filmorate.dao.GenreStorage;
 import ru.yandex.practicum.filmorate.exceptions.GenreNotFound;
 import ru.yandex.practicum.filmorate.model.Genre;
 
-import java.sql.ResultSet;
-import java.util.*;
+import java.util.List;
 
 @Repository
 @Slf4j
 public class GenreDaoStorageImpl implements GenreStorage {
-    final Map<Integer, Genre> genres;
     private final JdbcTemplate jdbcTemplate;
 
     @Autowired
     public GenreDaoStorageImpl(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
-        this.genres = loadValueFromDb();
-    }
-
-    private Map<Integer, Genre> loadValueFromDb() {
-        Map<Integer, Genre> genres = new HashMap<>();
-
-        String sql = "SELECT * FROM GENRES ORDER BY GENRE_ID;";
-        jdbcTemplate.query(sql, (ResultSet rs) -> {
-            do {
-                int genreId = rs.getInt("GENRE_ID");
-                genres.put(genreId
-                        , new Genre(genreId, rs.getString("NAME")));
-            } while (rs.next());
-        });
-        log.info("Load genres list size: {}", genres.size());
-        return genres;
     }
 
     @Override
-    public int indexOfValue(String genre) throws GenreNotFound {
-        Optional<Integer> index = genres.entrySet()
-                .stream()
-                .filter((e) -> e.getValue().getName().equals(genre))
-                .map(Map.Entry::getKey)
-                .findFirst();
-        if (index.isPresent()) {
-            return index.get();
+    @Cacheable("indexGenres")
+    public int indexOfValue(String genreName) throws GenreNotFound {
+        String sql = "SELECT GENRE_ID FROM GENRES WHERE NAME = ? ;";
+        List<Integer> genresIndex = jdbcTemplate.query(sql,
+                (rs, rowNum) -> rs.getInt("GENRE_ID")
+                , genreName);
+        if (genresIndex.size() > 0) {
+            return genresIndex.get(0);
         }
+
         throw new GenreNotFound("Genre Value not found.");
     }
 
     @Override
+    @Cacheable("genres")
     public Genre getGenreById(int genreId) throws GenreNotFound {
-        if (genres.containsKey(genreId)) {
-            return genres.get(genreId);
+        String sql = "SELECT * FROM GENRES WHERE GENRE_ID = ? ;";
+        List<Genre> genres = jdbcTemplate.query(sql,
+                (rs, rowNum) -> new Genre(rs.getInt("GENRE_ID"), rs.getString("NAME"))
+                , genreId);
+        if (genres.size() > 0) {
+            return genres.get(0);
         }
-        throw new GenreNotFound("Genre Index not exist.");
+        throw new GenreNotFound("Genre Index not found.");
     }
 
     public List<Genre> getFilmGenres(long filmId) {
@@ -69,7 +58,10 @@ public class GenreDaoStorageImpl implements GenreStorage {
     }
 
     @Override
-    public Collection<Genre> findAll() {
-        return genres.values();
+    @Cacheable("genresList")
+    public List<Genre> findAll() {
+        String sql = "SELECT * FROM GENRES ORDER BY GENRE_ID;";
+        return jdbcTemplate.query(sql,
+                (rs, rowNum) -> new Genre(rs.getInt("GENRE_ID"), rs.getString("NAME")));
     }
 }
