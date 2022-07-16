874cbaa00aabe6debc35ac32beead1a19cee6a2a
bannovdaniil <baddan@mail.ru>
2022-07-16 03:51:20 +0300

    #filmorate !11 тестирование FilmStorage

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -7,15 +7,14 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import ru.yandex.practicum.filmorate.dao.FilmStorage;
+import ru.yandex.practicum.filmorate.dao.MpaStorage;
 import ru.yandex.practicum.filmorate.dao.UserStorage;
 import ru.yandex.practicum.filmorate.dto.DtoFilm;
 import ru.yandex.practicum.filmorate.dto.DtoUser;
-import ru.yandex.practicum.filmorate.exceptions.InvalidEmailException;
-import ru.yandex.practicum.filmorate.exceptions.UserAlreadyExistException;
-import ru.yandex.practicum.filmorate.exceptions.UserNotFoundException;
-import ru.yandex.practicum.filmorate.exceptions.UserRemoveException;
+import ru.yandex.practicum.filmorate.exceptions.*;
 import ru.yandex.practicum.filmorate.mapper.DtoMapper;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.MpaRating;
 import ru.yandex.practicum.filmorate.model.User;
 
 import java.time.LocalDate;
@@ -42,8 +41,15 @@ class FilmorateApplicationTests {
     private Film film1;
     private Film film2;
 
+    private void clearDb() {
+        String sql = "DELETE FROM USERS; " +
+                " DELETE FROM FILMS;";
+        jdbcTemplate.update(sql);
+    }
+
     @BeforeEach
     void beforeEach() {
+        clearDb();
         dtoUser1 = new DtoUser();
         dtoUser1.setId(1);
         dtoUser1.setEmail("testemail1@mail.ru");
@@ -61,13 +67,34 @@ class FilmorateApplicationTests {
         dtoUser2.setBirthday(LocalDate.of(1991, Month.FEBRUARY, 10));
 
         user2 = dtoMapper.dtoToUser(dtoUser2);
+
+        dtoFilm1 = new DtoFilm();
+        dtoFilm1.setId(1L);
+        dtoFilm1.setName("testFilm1");
+        dtoFilm1.setDescription("description Of test film 1");
+        dtoFilm1.setDuration(100L);
+        dtoFilm1.setReleaseDate(LocalDate.of(2022, Month.JULY, 15));
+        dtoFilm1.setRate(1L);
+        dtoFilm1.setMpa(new MpaRating(1, "G"));
+
+        film1 = dtoMapper.dtoToFilm(dtoFilm1);
+
+        dtoFilm2 = new DtoFilm();
+        dtoFilm2.setId(2L);
+        dtoFilm2.setName("testFilm2");
+        dtoFilm2.setDescription("description Of test film 2");
+        dtoFilm2.setDuration(100L);
+        dtoFilm2.setReleaseDate(LocalDate.of(2022, Month.JULY, 10));
+        dtoFilm2.setRate(1L);
+        dtoFilm2.setMpa(new MpaRating(2, "PG"));
+
+        film2 = dtoMapper.dtoToFilm(dtoFilm2);
+
     }
 
     @AfterEach
     void afterEach() {
-        String sql = "DELETE FROM USERS;";
-        jdbcTemplate.update(sql);
-
+        clearDb();
     }
 
     @DisplayName("Create User")
@@ -135,6 +162,74 @@ class FilmorateApplicationTests {
         List<User> userListResult = userStorage.findAll();
 
         Assertions.assertEquals(userListExpected, userListResult);
+    }
+
+
+    @DisplayName("Create Film")
+    @Test
+    public void createFilm() throws InvalidFilmException, UserAlreadyExistException, GenreNotFound, MpaRatingNotFound, MpaRatingNotValid {
+        Film filmResult = filmStorage.create(dtoFilm1);
+
+        assertThat(filmResult).hasFieldOrPropertyWithValue("name", "testFilm1");
+    }
+
+    @DisplayName("Find Film by Id")
+    @Test
+    public void testFindFilmById() throws FilmNotFoundException, InvalidFilmException, UserAlreadyExistException, GenreNotFound, MpaRatingNotFound, MpaRatingNotValid {
+        Film film = filmStorage.create(dtoFilm1);
+
+        long filmId = film.getId();
+
+        Film filmResult = filmStorage.getFilmById(filmId);
+
+        assertThat(film)
+                .hasFieldOrPropertyWithValue("id", filmId)
+                .hasFieldOrPropertyWithValue("name", "testFilm1");
+    }
+
+    @DisplayName("Update Film")
+    @Test
+    public void updateFilm() throws FilmNotFoundException, InvalidFilmException, UserAlreadyExistException, GenreNotFound, MpaRatingNotFound, MpaRatingNotValid {
+        Film filmResult = filmStorage.create(dtoFilm1);
+
+        long filmId = filmResult.getId();
+        dtoFilm1.setId(filmId);
+        dtoFilm1.setName("EDITtestFilm1");
+
+        filmStorage.update(dtoFilm1);
+
+        filmResult = filmStorage.getFilmById(filmId);
+
+        assertThat(filmResult).hasFieldOrPropertyWithValue("name", "EDITtestFilm1");
+    }
+
+    @DisplayName("Remove Film")
+    @Test
+    public void removeFilm() throws InvalidFilmException, UserAlreadyExistException, GenreNotFound, MpaRatingNotFound, MpaRatingNotValid, InvalidFilmRemoveException {
+        Film filmResult = filmStorage.create(dtoFilm1);
+
+        long filmId = filmResult.getId();
+        dtoFilm1.setId(filmId);
+
+        filmStorage.remove(dtoFilm1);
+
+        FilmNotFoundException exception = Assertions.assertThrows(FilmNotFoundException.class,
+                () -> filmStorage.getFilmById(filmId)
+        );
+
+        Assertions.assertEquals("Film ID not found.", exception.getMessage());
+    }
+
+    @DisplayName("Find All Films")
+    @Test
+    public void findAllFilm() throws InvalidFilmException, UserAlreadyExistException, GenreNotFound, MpaRatingNotFound, MpaRatingNotValid {
+        Film filmResult1 = filmStorage.create(dtoFilm1);
+        Film filmResult2 = filmStorage.create(dtoFilm2);
+
+        List<Film> filmListExpected = List.of(film1, film2);
+        List<Film> filmListResult = filmStorage.findAll();
+
+        Assertions.assertEquals(filmListExpected, filmListResult);
     }
 
     @Test
