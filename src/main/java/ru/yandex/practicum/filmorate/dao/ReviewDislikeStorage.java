0c1c07fde91f53725b048c0a97ecd7c679b432df
bannovdaniil <baddan@mail.ru>
2022-07-19 10:44:22 +0300

    #filmorate-group: add: Review DisLike Storage interface.

---
+++ src/main/java/ru/yandex/practicum/filmorate/dao/ReviewDislikeStorage.java
@@ -0,0 +1,11 @@
+package ru.yandex.practicum.filmorate.dao;
+
+import ru.yandex.practicum.filmorate.exceptions.UserNotFoundException;
+
+public interface ReviewDislikeStorage {
+    void addDislike(long reviewId, long userId) throws ReviewNotFoundException, UserNotFoundException;
+
+    void removeDislike(long reviewId, long userId) throws ReviewNotFoundException, UserNotFoundException;
+
+    long getDislikeCount(long reviewId);
+}
