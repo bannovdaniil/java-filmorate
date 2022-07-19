e27ac6667fa4b60dfc9fef8412c34a5c68695d01
bannovdaniil <baddan@mail.ru>
2022-07-19 12:53:17 +0300

    #filmorate-group: refactoring: Review model add LomBok сеттеры, геттеры и конструкторы.

--- src/main/java/ru/yandex/practicum/filmorate/model/Review.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Review.java
@@ -1,19 +1,26 @@
 package ru.yandex.practicum.filmorate.model;
 
+import lombok.*;
+
 /**
  * {
- *     “reviewId”: 123,
- *     “content”: “This film is sooo baad.”,
- *     “isPositive”: false,
- *     “userId”: 123, // Пользователь
- *     “filmId”: 2, // Фильм
- *     “useful”: 20 // рейтинг полезности
+ * “reviewId”: 123,
+ * “content”: “This film is sooo baad.”,
+ * “isPositive”: false,
+ * “userId”: 123, // Пользователь
+ * “filmId”: 2, // Фильм
+ * “useful”: 20 // рейтинг полезности
  * }
  */
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class Review {
     private long reviewId;
     private String content;
-    private boolean isPositive;
+    private boolean positive;
     private long userId;
     private long filmId;
     private int useful;
